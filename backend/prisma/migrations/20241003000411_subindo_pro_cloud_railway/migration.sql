92f1c3216e28b57b29c63e5439334aa998a6a0ac
AndersonN031 <devandersonnunes078@gmail.com>
2024-10-03 06:35:35 -0300

    vercel file

--- backend/prisma/migrations/20241003000411_subindo_pro_cloud_railway/migration.sql
+++ backend/prisma/migrations/20241003000411_subindo_pro_cloud_railway/migration.sql
@@ -1,20 +1,12 @@
-/*
-  Warnings:
-
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `users`;
-
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
-    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'ADMIN',
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
 
+    UNIQUE INDEX `User_name_key`(`name`),
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
